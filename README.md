f95318243d21b75b64aa8908fcdb1282a3858e24
arykanary <74874433+arykanary@users.noreply.github.com>
2023-05-06 09:30:33 +0200

    Add code

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# SmartBattery
-SmartBattery is a repository for code regarding ultra simple concept for a home battery
+# SmartBatterySystem
+
+The smart battery uses low-cost electricity to charge the battery.
+This electricity can then be used when elekctricity is expensive.
+The key to it's succes should be based on simplicity, sustainability and low-impact.
+All the system that rely on the battery should be able to function without any alterations.
+
+# Physical system
+
+The physical part consists of four items:
+
+1. Batteries
+2. Converter -> battery to grid voltage
+3. Control system (see below)
+4. Converter -> grid to battery voltage
+
+It would be more efficient if a 2-way convert would be used but they don't seem to exist.
+An integrated system would be even more efficient and solve previous problem. But this initially makes it less simple.
+
+The components used in the system are:
+- Raspberry PI 4, with the follow compontents:
+    - A relay: https://www.bitsandparts.nl/Relais-board-1-kanaals-5V-optocoupled-p101104
+    - A voltmeter: https://www.bitsandparts.nl/Spanningsmeter-spanningsdeler-tot-25V-p1096274
+    - Wiring
+    - Indicator LED's
+- An inverter: https://www.bol.com/nl/nl/p/ecoline-omvormer-12v-naar-220v-230v-300w-600w-vermogen-gemodificeerde-sinus-auto-omvormer/9300000134186311/?s2a=
+- A charger: https://www.bol.com/nl/nl/p/tecmate-optimate-4-dual-can-bus-0-8a-12v-acculader-druppellader-voor-scooter-motor/9200000090601966/?
+
+# Control system
+
+The control system is responsible for making sure the systems that rely on the battery get low-cost electricity.
+This means that it should use several datasources to get low-cost electricity and save it for later.
+Datasources that could be beneficial:
+
+- Battery charge (voltage)
+- Solarpanel charge
+- Day/Night electricity (when having 2 two tariff's)
+- Electricity price (when having a flexible price)
+
+Preferably there should be a way to predict all of the above in order to increase efficiency.
+
+## Physical part of the control system
+
+The control system is as can be seen from the component list nothing more that a relay, voltmeter and some indicator leds.
+This is simple as intented.
---
+++ main.py
@@ -0,0 +1,262 @@
+import re
+import serial
+from datetime import datetime
+import pandas as pd
+import time
+import os
+import RPi.GPIO as GPIO
+
+
+# == Data-source objects ==
+class EexApi:
+    """This class handles all interactions with the EEX API to get the data about current prices, volumnes, etc.
+    https://www.eex.com/en/market-data/eex-group-datasource/api
+    """
+    # ToDo: API is fucking expensive
+    base_url = "https://api.eex.com/data/spot/power/monthly/auction/nl"
+
+    def __init__(self):
+        pass
+
+    def call_api(self):
+        response = requests.get(self.base_url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return response.status_code
+        
+    def retrieve_value(self, name):
+        pass
+        
+
+class SmartMeter:
+    """This class handles all interactions with the local smart meter to get the data about current use, day/night, etc."""
+    def __init__(self, port="COM4", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False):
+        self.port, self.baudrate, self.bytesize, self.parity = port, baudrate, bytesize, parity
+        self.stopbits, self.timeout, self.xonxoff, self.rtscts = stopbits, timeout, xonxoff, rtscts
+
+        self.mapper = pd.read_excel('Definitions.xlsx', 'Sheet1', index_col='OBIS reference')
+        self.mapper.index = self.mapper.index.map(lambda x: x.replace('.255', ''))
+
+        self.serial_out = []
+        self.reading = pd.DataFrame()
+
+    def read_meter(self):
+        out = []
+        with serial.Serial(
+            port=self.port,
+            baudrate=self.baudrate,
+            bytesize=self.bytesize,
+            parity=self.parity,
+            stopbits=self.stopbits,
+            timeout=self.timeout,
+            xonxoff=self.xonxoff,
+            rtscts=self.rtscts,
+        ) as ser:
+            while True:
+                telegram_line = ser.readline()
+                out.append(telegram_line.decode('ascii').strip())
+
+                if re.match(b'(?=!)', telegram_line):
+                    break
+                
+        self.serial_out = out
+    
+    def transform(self, tel):
+        """Converts tel to a list of key-value pairs"""
+        try:
+            trans = [x for x in self.mapper.index if x in tel][0]
+            name = self.mapper.loc[[trans], 'Short name'].values
+        except (ValueError, IndexError):
+            return list()
+        
+        tel = re.findall(r'\((.*?)\)', tel)
+        ntel = []
+        for t in tel:
+            t = t.split('*')
+            if len(t) == 1:
+                t += ['-']
+            ntel.append(t)
+        
+        return list(zip(name, *zip(*ntel)))
+
+    def reading2df(self):
+        df = pd.DataFrame([x for item in self.serial_out for x in self.transform(item)])\
+            .rename({0: 'Name', 1: 'Value', 2: 'Unit'}, axis=1)\
+            .set_index('Name')
+
+        dts = [x for x in df.index if 'DateTime' in x]
+        dtv = [datetime.strptime(x.replace('S', ''), r'%Y%m%d%H%M%S') for x in df.loc[dts, 'Value'].values]
+        for s, v in zip(dts, dtv):
+            df.loc[s, 'Value'] = v
+
+        self.reading = df
+
+    def update_log(self, logdir='_data/P1_log'):
+        """"""
+        date = self.reading.loc['DateTimeElectric', 'Value']
+        self.reading.to_csv(f'{logdir}/{date}.csv')
+
+    def __call__(self, names=['DateTimeElectric', 'ActualElectricityToClient', 'ActualElectricityByClient'], save=True):
+        """"""
+        self.read_meter()
+        self.reading2df()
+        self.update_log()
+        return self.reading.loc[names]
+        
+
+class SolarPanel:
+    """This class handles all interactions with the solar-panel control system to get the data about current charge, state, etc."""
+    # ToDo: Can start this once the solarpanel are in place.
+    def __init__(self):
+        pass
+
+
+class Predict:
+    """This class can be used to attempt to predict power usage, price, etc."""
+    def __init__(self):
+        pass
+
+
+# == RPI interaction objects ==
+class RpiBoard:
+    """This class creates a sustainable way of setting up the board.
+    
+    https://sourceforge.net/p/raspberry-gpio-python/wiki/Home/
+    """
+    pins = [
+    # 'BoardNo', 'BCMNo', 'Type'
+        (1,  None, '3.3V'),
+        (2,  None, '5.0V'),
+        (3,  2,    ('GPIO', 'SDA1', 'I2C')),
+        (4,  None, '5.0V'),
+        (5,  3,    ('GPIO', 'SDA1', 'I2C')),
+        (6,  None, 'GROUND'),
+        (7,  4,    ('GPIO', 'GCLK')),
+        (8,  14,   ('GPIO', 'TX UART')),
+        (9,  None, 'GROUND'),
+        (10, 15,   ('GPIO', 'RX UART')),
+        (11, 17,   'GPIO'),
+        (12, 18,   'GPIO'),
+        (13, 27,   'GPIO'),
+        (14, None, 'GROUND'),
+        (15, 22,   'GPIO'),
+        (16, 23,   'GPIO'),
+        (17, None, '3.3V'),
+        (18, 24,   'GPIO'),
+        (19, 20,   ('MOSI', 'SPI')),
+        (20, None, 'GROUND'),
+        (21, 9,    ('MISO', 'SPI')),
+        (22, 25,   'GPIO'),
+        (23, 11,   ('SCLK', 'SPI')),
+        (24, 8,    ('CEO_N', 'SPI')),
+        (25, None, 'GROUND'),
+        (26, 7,    ('CE1_N', 'SPI')),
+        (27, None, 'RESERVED'),
+        (28, None, 'RESERVED'),
+        (29, 5,    'GPIO'),
+        (30, None, 'GROUND'),
+        (31, 6,    'GPIO'),
+        (32, 12,   'GPIO'),
+        (33, 13,   'GPIO'),
+        (34, None, 'GROUND'),
+        (35, 19,   'GPIO'),
+        (36, 16,   'GPIO'),
+        (37, 26,   'GPIO'),
+        (38, 20,   'GPIO'),
+        (39, None, 'GROUND'),
+        (40, 21,   'GPIO'),
+    ]
+
+    def __init__(self, mode):
+        self.mode = mode
+        self.active_pins = {}
+
+    def __enter__(self):
+        GPIO.setmode(self.mode)
+
+    def __exit__(self, *args, **kwargs):
+        GPIO.cleanup()
+    
+    @property
+    def board(self):
+        return GPIO.RPI_INFO
+    
+
+class RpiPin:
+    """This class can be used to easily interact with a pin on a RPI"""
+    def __init__(self, pin: int):
+        self._pin = pin
+        self._func = None
+    
+    @property
+    def state(self):
+        return GPIO.input(self._pin)
+
+    states = [GPIO.IN, GPIO.OUT, GPIO.SPI, GPIO.I2C, GPIO.HARD_PWM, GPIO.SERIAL, GPIO.UNKNOWN]
+    @state.setter
+    def state(self, state):
+        GPIO.output(self._pin, state)
+    
+    @property
+    def function(self):
+        self._func = GPIO.gpio_function(self._pin)
+        return self._func
+    
+    @function.setter
+    def function(self, func):
+        self._func = func
+        GPIO.setup(self._pin, func)
+
+
+# == Combine it all ==
+def run(freq=5):
+    """combine everything and run in a loop"""
+    with RpiBoard(GPIO.BOARD) as rpi_board:
+        while True:
+            charge = True
+
+            #### Read all the data points ####
+            smartm = SmartMeter(port="/dev/ttyUSB0", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False)  # RPI4
+            date, to, by = smartm()
+            
+            charge &= (by - to + 50) > 0
+
+            #### Define the board and pins ####
+            rel_pin = RpiPin(11)
+            rel_pin.function = GPIO.OUT
+
+            rel_pin.state = not rel_pin.state
+
+            time.sleep(freq)
+
+
+if __name__ == "__main__":
+    # with RpiBoard(GPIO.BOARD) as rpi_board:
+    #     led_pin = RpiPin(40)
+    #     led_pin.function = GPIO.OUT
+
+    #     rel_pin = RpiPin(11)
+    #     rel_pin.function = GPIO.OUT
+
+    #     print('init state', led_pin.state, rel_pin.state)
+
+    #     while True:
+    #         led_pin.state = not led_pin.state
+    #         rel_pin.state = not rel_pin.state
+    #         print(led_pin.state, rel_pin.state)
+    #         c = input('continue?')
+    #         if len(c)>0:
+    #             break  
+
+    # smartm = SmartMeter(port="COM4", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False)  # Windows
+    # smartm = SmartMeter(port="/dev/ttyUSB0", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False)  # RPI4
+    # print(smartm())
+
+    pass  # everything before the pass has been tested and works.
+    # run()
+
+    with RpiBoard(GPIO.BOARD) as rpi_board:
+        led_pin = RpiPin(40)
+        led_pin.function = GPIO.IN
