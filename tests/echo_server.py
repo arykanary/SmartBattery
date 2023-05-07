ed40cb2fbe9e45bfa8790da5d96f0f042eef0473
arykanary <wouter.kramer@me.com>
2023-05-07 14:53:52 +0200

    port/socket communication

---
+++ tests/echo_server.py
@@ -0,0 +1,18 @@
+# echo_server.py
+
+import socket
+
+HOST = "192.168.68.58"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if data:
+                print(data)
+            conn.sendall('This is a test message')
