c83318f1d2176691545af850b0ef05f67f38af4f
arykanary <74874433+arykanary@users.noreply.github.com>
2023-05-07 20:58:35 +0200

    com reading

--- run_rpiz1.py
+++ run_rpiz1.py
@@ -2,9 +2,39 @@
 hostname : 192.168.68.55
 """
 from main import *
+import socket
+from signal import signal, SIGPIPE, SIG_DFL
 
+signal(SIGPIPE,SIG_DFL)
 
-if __name__ == "__main__":
-    # smartm = SmartMeter(port="COM4", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False)  # Windows
-    smartm = SmartMeter(port="/dev/ttyUSB0", baudrate=115200, bytesize=8, parity="N", stopbits=1, timeout=5, xonxoff=False, rtscts=False)  # RPI4
-    print(smartm())
+HOST = "192.168.68.55"  # Standard loopback interface address (localhost, RPI Zero 1)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if data:
+                data = data.decode('ascii')
+                print('Client wants', data)
+
+                smartm = SmartMeter(
+                    port="/dev/ttyUSB0",
+                    baudrate=115200,
+                    bytesize=8,
+                    parity="N",
+                    stopbits=1,
+                    timeout=5,
+                    xonxoff=False,
+                    rtscts=False
+                )
+                _return = smartm([data]).values[0][0].encode('ascii')
+
+                conn.sendall(_return)
+
+    print(f"Received {data!r}")
--- tests/echo_client.py
+++ tests/echo_client.py
@@ -2,13 +2,25 @@
 
 import socket
 
-HOST = "192.168.68.58"  # Standard loopback interface address (localhost, RPI 4)
-# HOST = "192.168.68.55"  # Standard loopback interface address (localhost, RPI Zero 1)
+# HOST = "192.168.68.58"  # Standard loopback interface address (localhost, RPI 4)
+HOST = "192.168.68.55"  # Standard loopback interface address (localhost, RPI Zero 1)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    # P1Version, DateTimeElectric, EquipmentIDElectric,
+    # ElectricityToClient(1), ElectricityToClient(2), ElectricityByClient(1), ElectricityByClient(2),
+    # TariffIndicator, ActualElectricityToClient, ActualElectricityByClient,
+    # NPowerFailuresAny, NLongPowerFailuresAny, LongPowerFailureLog, NSagsL1, NSagsL2, NSagsL3,
+    # NSwellsL1, NSwellsL2, NSwellsL3, Message, InstantVoltageL1, InstantVoltageL2, InstantVoltageL3,
+    # InstantCurrentL1, InstantCurrentL2, InstantCurrentL3,
+    # Instant+PowerL1, Instant+PowerL2, Instant+PowerL3,
+    # Instant-PowerL1, Instant-PowerL2, Instant-PowerL3,
+    # DeviceTypeGas, EquipmentIDGas, DateTimeGas, GasToClient, DeviceTypeThermal, EquipmentIDThermal,
+    # DateTimeThermal, ThermalToClient, DeviceTypeWater, EquipmentIDWater, DateTimeWater,
+    # WaterToClient, DeviceTypeSlave, EquipmentIDSlave, DateTimeSlave, SlaveToClient
+
+    s.sendall(b"ElectricityToClient(1)")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
