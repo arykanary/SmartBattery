665d09890100c62245ed692bb2e17c44cc3a3f72
arykanary <74874433+arykanary@users.noreply.github.com>
2024-10-06 10:33:06 +0200

    api

---
+++ smart_battery/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from time import sleep
+from .main import SmartMeter
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    smartm = SmartMeter(
+        '/dev/ttyUSB0',
+        115200,
+        8,
+        'N',
+        1,
+        5,
+        False,
+        False
+    )
+    while True:
+        try:
+            return smartm()
+        except (KeyError, ValueError):
+            print('Failed to get data')
+            sleep(1)
