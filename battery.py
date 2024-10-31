eabaa05552517be3b9dd0b6b4885d45a42350727
arykanary <74874433+arykanary@users.noreply.github.com>
2024-10-31 13:36:14 +0100

    6 relais should be working now

--- battery.py
+++ battery.py
@@ -1,16 +1,31 @@
 from time import sleep
 import RPi.GPIO as GPIO
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from main import CheckCharge, RpiBoard, RpiPin
 
 
 # Pins
-charge_pin = RpiPin(4)
-bypass_pin = RpiPin(5)
-charge_pin.function = GPIO.OUT
-bypass_pin.function = GPIO.OUT
+charge_min_pin = RpiPin(17)
+charge_max_pin = RpiPin(27)
+charge_min_pin.function = GPIO.OUT
+charge_max_pin.function = GPIO.OUT
 
+bypass_min_pin = RpiPin(5)
+bypass_max_pin = RpiPin(6)
+bypass_min_pin.function = GPIO.OUT
+bypass_max_pin.function = GPIO.OUT
+
+convert_min_pin = RpiPin(23)
+convert_max_pin = RpiPin(24)
+convert_min_pin.function = GPIO.OUT
+convert_max_pin.function = GPIO.OUT
+
+bypass_min_pin.state = bypass_min_pin.state = 1
+charge_min_pin.state = charge_max_pin.state = 1
+convert_min_pin.state = convert_max_pin.state = 1
+
+# Determine current state each 15 seconds
 cc = CheckCharge(
     threshold_bypass=.1,
     threshold_charge=.1,
@@ -23,11 +38,9 @@ while True:
     bypass, charge, both = cc()
 
     # State changes
-    if both:
-        charge_pin.state = 0
-        bypass_pin.state = 0
-    else:
-        charge_pin.state = 1
-        bypass_pin.state = 1
+    charge_min_pin.state = charge_max_pin.state = charge
+    bypass_min_pin.state = bypass_min_pin.state = both
+    
+    convert_min_pin.state = convert_max_pin.state = not bypass_min_pin.state
 
     sleep(15)
